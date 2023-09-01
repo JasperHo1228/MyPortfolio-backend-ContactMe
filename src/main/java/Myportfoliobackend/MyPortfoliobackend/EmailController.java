413b4e603211f9007313db8b5e80e85afa71bd50
Jasper Ho <49902173+JasperHo1228@users.noreply.github.com>
2023-09-01 22:56:52 +0100

    enhance security

--- src/main/java/Myportfoliobackend/MyPortfoliobackend/EmailController.java
+++ src/main/java/Myportfoliobackend/MyPortfoliobackend/EmailController.java
@@ -1,6 +1,7 @@
 package Myportfoliobackend.MyPortfoliobackend;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
@@ -16,23 +17,27 @@ public class EmailController {
     @Autowired
     private final JavaMailSender javaMailSender;
 
+    @Value("${spring.mail.username}")
+    private String defaultEmail;
+
     public EmailController(JavaMailSender javaMailSender){
         this.javaMailSender = javaMailSender;
     }
     @PostMapping("/send-email")
     public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request) {
         String sender = request.getSender(); // Replace with your email address
-        String name = request.getName() + " send you a message from your portfolio";
+        String name = request.getName();
         String message = "Hi Jasper,\n\n"
                           + request.getMessage() +
                           "\n\nIf you are interested, please feel welcome to reach out to me at my email " +
                             sender +
                           "\n\nKind regards,\n\n"
-                          + request.getName();
+                          + name;
+        String subject = name + " send you a message from your portfolio";
 
         SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo("tsunyinho1996@gmail.com");
-        mailMessage.setSubject(name);
+        mailMessage.setTo(defaultEmail);
+        mailMessage.setSubject(subject);
         mailMessage.setText(message);
 
         try {
